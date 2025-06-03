c943ed8d863dedbe831fd8ebdb811a6ca9827886
jenilchevli <jenilchevli007@gmail.com>
2025-06-03 17:46:58 +0530

    which-number_is_bigger

---
+++ 5th.py
@@ -0,0 +1,38 @@
+a = int(input(" Enter the value of a ") )
+b = int(input(" Enter the value of b "))
+c = int(input(" Enter the value of c "))
+d = int(input(" Enter the value of d "))
+
+if a >= b:
+    if a >= c:
+        if a >= d:
+            if a >= a:
+             print("A is biggest number form the all the number")
+             
+else:
+    print("invailed number form all the number")
+
+if b >= a:
+    if b >= c:
+        if a >= d:
+            if b >= b :
+                print("B is biggest number form the all the number")
+else:
+    print("invailed number form all the number")
+
+if c >= a:
+    if c >= b:
+        if c >= d:
+            if c >= c :
+                print("C is biggest number form the all the number")
+                
+else:
+    print("invailed number form all the number")
+
+if d >= a:
+    if d >= b:
+        if d >= c:
+            if d >= d :
+                print("D is biggest number form the all the number")
+else:
+    print("invailed number form all the number")
